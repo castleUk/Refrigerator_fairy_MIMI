193bc7b92fe35b34f483e20c030e925e9c26980d
DESKTOP-8N3GG4O\GGG <jylee22.96@gmail.com>
2023-01-20 15:02:32 +0900

    스타일시트 수정

--- react_front/src/components/nutrient/modals/NutrientModal.jsx
+++ react_front/src/components/nutrient/modals/NutrientModal.jsx
@@ -3,6 +3,7 @@ import RecipesGroup from "../../recipe/RecipesGroup";
 import NutrientTable from "../NutrientTable";
 import Button from "react-bootstrap/Button";
 import { instance } from "../../api/Api";
+import { AiFillDelete } from "react-icons/ai";
 
 const NutrientModal = ({
   show,
@@ -38,36 +39,41 @@ const NutrientModal = ({
         show={show}
         onHide={onHide}
         className="ingr-detail-modal"
+        centered
       >
         <Modal.Header closeButton>
-          <img
-            className="ingr-img"
-            src={itemInfo.item.img}
-            alt={itemInfo.item.name}
-          />
-
-          <div className="detail-ingr">
-            <h5 className="title">{itemInfo.item.name}</h5>
-            {/* <span className="count-text">갯수 : {itemInfo.count}</span> */}
-          </div>
-          <Button
-            onClick={() => {
-              deleteHandler();
-              onHide();
-            }}
-          >
-            삭제
-          </Button>
+          <Modal.Title>재료 상세보기</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
+          <div className="content-header">
+            <img
+              className="ingr-img"
+              src={itemInfo.item.img}
+              alt={itemInfo.item.name}
+            />
+
+            <div className="detail-ingr">
+              <h5 className="title">{itemInfo.item.name}</h5>
+              {/* <span className="count-text">갯수 : {itemInfo.count}</span> */}
+            </div>
+            <Button
+              variant="danger"
+              onClick={() => {
+                deleteHandler();
+                onHide();
+              }}
+            >
+              <AiFillDelete className="icon" />
+            </Button>
+          </div>
+
           <NutrientTable itemInfo={itemInfo.item} />
           <h5>메뉴 추천</h5>
           <div className="content-body">
             <RecipesGroup itemName={itemInfo.item.name} />
           </div>
         </Modal.Body>
-        <Modal.Footer></Modal.Footer>
       </Modal>
     </>
   );
