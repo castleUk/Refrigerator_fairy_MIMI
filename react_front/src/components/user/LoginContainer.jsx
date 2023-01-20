60d48c7afd931ba2ed3e6c9745145922d89b1050
notebook <ppkk24@naver.com>
2023-01-20 18:43:52 +0900

    수정

--- react_front/src/components/user/LoginContainer.jsx
+++ react_front/src/components/user/LoginContainer.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const LoginContainer = () => {
   const navigate = useNavigate();
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
 
   //로그인
   const onLogin = async (account) => {
@@ -14,7 +14,7 @@ const LoginContainer = () => {
       userPw: account.userPw,
     };
 
-    console.log(loginInfo);
+    console.log(JSON.stringify(loginInfo));
     try {
       const response = await axios.post(
         "/auth/login",
@@ -25,15 +25,15 @@ const LoginContainer = () => {
       localStorage.setItem("accessToken", data.accessToken);
       localStorage.setItem("refreshToken", data.refreshToken);
       alert("로그인 성공!");
-      navigate("/freezer",{ replace: true})
+      navigate("/freezer", { replace: true });
     } catch (error) {
-     if(error.response.status === 401){
-      console.log("에러 인데요")
-      setError("아이디 or 비밀번호를 확인하세요")
-     }
+      if (error.response.status === 400) {
+        console.log("에러 인데요");
+        setError("아이디 or 비밀번호를 확인하세요");
+      }
+    }
   };
-  }
-  return <LoginForm onLogin={onLogin} onError={error}/>;
+  return <LoginForm onLogin={onLogin} onError={error} />;
 };
 
 export default LoginContainer;
